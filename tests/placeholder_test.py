e415ed973334e568b8023c10783d807cd09f490e
Salil Apte <salil.apte99@gmail.com>
2024-12-07 09:14:27 -0500

    Update test.py

--- tests/placeholder_test.py
+++ tests/placeholder_test.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
